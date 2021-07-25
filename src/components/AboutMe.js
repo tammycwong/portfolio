6efd6afd5611325ff5ed6a21879765fca42e940b
Tammy <tammycwong@gmail.com>
2021-07-25 18:44:49 -0400

    header

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,8 +3,15 @@ import React from 'react';
 function AboutMe() {
 
     return (
-    <div>ABOUT ME:
-    <p>Hello!</p>
+    <div>
+    <h1>Hello!</h1>
+    <h2>I am Tammy Wong and I am a</h2> <h3 class="typing-demo">software developer.</h3>
+    {/* <p>
+    Passionate. Honest. Hard-worker.
+    <br/>
+    I love creating applications that are fun and users can benefit from.<br/>
+    I first started coding when I was 15, personalizing my layouts for my blog platforms- Xanga and LiveJournal. <br/>
+    I loved changing themes to my site through custom banners, mp3s and dynamic styling with JavaScript and CSS. </p> */}
     </div>
     
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Link from 'react-router-dom';
 
 function NavBar() {
 
     return (
-    <div>NavBar:</div>
+    <div className="navbar">
+        {/* <Link to="about">
+            About Me
+        </Link>
+        <Link to="Projects">
+            Projects
+        </Link>
+        <Link to="Contact">
+            Contact
+        </Link> */}
+
+    </div>
     
     )
 }
