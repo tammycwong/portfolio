d69ae79a371f3b04fc4751f831fd56ab69f73b3d
Tammy <tammycwong@gmail.com>
2021-07-27 23:39:36 -0400

    need to update about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,9 +8,16 @@ function AboutMe() {
       <h2>About Me</h2>
       <div className="aboutme-container">
       <p className="aboutme">
+      A career changer from hospitality management to tech.<br/>
+      I have a serious passion for creating fun and beneficial applications;<br/>
+      building an engaging user experience and learning the best way to do so, i find, is the most exhilarating journey.<br/>
+      {/* One of my strengths is frontend engineering as well as conceptual thinking. <br/> */}
+
       I first started coding when I was 15, personalizing my layouts for my blog platforms- Xanga and LiveJournal. <br/>
       I loved changing themes to my site through custom banners, mp3s and dynamic styling with JavaScript and CSS. <br/>
-      I love to build fun and very mildly (but sometimes moderately) marketable projects.<br/>
+
+      My background in cognitive and behaviorial psychology has helped me understand the human mind and how it works with user experience design.<br/>
+
       </p>
       </div>
     </div>
