8f9e16436f6392281b7bc604237d691fa837c7c7
Tammy <tammycwong@gmail.com>
2021-07-28 12:59:07 -0400

    contact

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,13 +9,14 @@ function AboutMe() {
       <div className="aboutme-container">
       <p className="aboutme">
       A career changer from hospitality management to tech.<br/>
-      I have a serious passion for creating fun and beneficial applications;<br/>
-      building an engaging user experience and learning the best way to do so, i find, is the most exhilarating journey.<br/>
+      I have a serious passion for creating fun yet beneficial applications;<br/>
+      The most exhilarating journey is building an engaging user experience and learning the best way to do so.<br/>
       {/* One of my strengths is frontend engineering as well as conceptual thinking. <br/> */}
-
+      <br/>
       I first started coding when I was 15, personalizing my layouts for my blog platforms- Xanga and LiveJournal. <br/>
       I loved changing themes to my site through custom banners, mp3s and dynamic styling with JavaScript and CSS. <br/>
-
+      Each layout taylored to my current mood as an angsty teen.<br/>
+      <br/>
       My background in cognitive and behaviorial psychology has helped me understand the human mind and how it works with user experience design.<br/>
 
       </p>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,31 @@ import React from 'react';
 function Contact() {
 
     return (
-    <div className="header">
-        Contact:
+    <div className="contact-container">
+        <h2>Contact</h2>
+        <br/>
+        <h3>Have a question or want to work together?</h3>
+        <br/>
+        <br/>
+        <div id="contact">
+        <form id="contact-form" action="mailto:tammycwong@gmail.com" method="post"
+        encrypt="text/plain">
+       
+        <input class="input-field" type="text" placeholder="Name" />
+        <br/>
+        
+        <input class="input-field" type="email" placeholder="Enter email" />
+        <br/>
+        
+        <textarea class="input-field" type="text" placeholder="Message" />
+        <br/>
+        <input id="submit-button" type="submit" value="Submit" />
+        </form>
+        </div>
+        </div>
 
-    </div>
+        
+
     
     )
 }
