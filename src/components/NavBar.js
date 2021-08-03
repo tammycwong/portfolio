833b8e0876c87f692ec840e26869e13c7d5512e7
Tammy <tammycwong@gmail.com>
2021-08-03 00:02:36 -0400

    positioned

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,13 +5,13 @@ function NavBar() {
 
     return (
     <div className="navbar">
-        <Link to="about-section" smooth={true} duration={1000}>
+        <Link to="about-section" smooth={true} duration={1000} >
             About Me
         </Link>
-        <Link to="projects-section" smooth={true} duration={1000}>
+        <Link to="projects-section" smooth={true} duration={1000} >
             Projects
         </Link>
-        <Link to="contact-section" smooth={true} duration={1000}>
+        <Link to="contact-section" smooth={true} duration={1000} >
             Contact
         </Link>
         <a href="https://drive.google.com/file/d/1EygD0hKfBJICHhkxrskBP6AUkLYT3fsp/view?usp=sharing" className="resume" target="_blank" rel="noreferrer">Resume</a>
